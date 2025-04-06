e78a6e27a1e1118106a88e2912850be15252ecdb
yoz75 <levtannn@gmail.com>
2025-04-06 19:56:46 +0300

    reduced memory usage and perfomance
    neural life don`t use alfa channel so i deleted it. Also arrays will be smaller so gc pressure is lower

--- Simulation/Color.cs
+++ Simulation/Color.cs
@@ -5,29 +5,26 @@ namespace NeuralLife.Simulation
 {
     public struct Color
     {
-
+        public const int SelfSize = 3;
+        public byte R;
+        public byte G;
+        public byte B;
         public Color()
         {
 
         }
 
-        public Color(byte r, byte g, byte b, byte a = 255)
+        public Color(byte r, byte g, byte b)
         {
             R = r;
             G = g;
             B = b;
-            A = a;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public uint AsUint()
         {
-            return (uint)(R << 24 | G << 16 | B << 8 | A << 0);
+            return (uint)(R << 24 | G << 16 | B << 8 | 255);
         }
-
-        public byte R;
-        public byte G;
-        public byte B;
-        public byte A;
     }
 }
